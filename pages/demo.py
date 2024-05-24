9429f1bd7116875f71da7be30d49b0060a092bbb
mohit <mohit21339@iiitd.ac.in>
2024-05-25 00:43:30 +0530

    updated demo.py

--- pages/demo.py
+++ pages/demo.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from keras.models import load_model
 import tensorflow as tf
+from tensorflow.keras.models import load_model
 import pandas as pd
 import numpy as np
 import cv2
